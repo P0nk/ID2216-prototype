4f464d04ad04cf69e10bf86b125179ab2d5234c2
Per Lovén <loven.per@gmail.com>
2018-11-18 19:51:40 +0100

    Clean up

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 function showSearchItems(){
 	document.getElementById("searchedItemsList").style.display = "block";
 }
+
+
+$(document).on("pageinit", function() {
+	$('video').on("play", function() {
+		setTimeout(function(){
+			console.log("test");
+			//TODO: change the progress bar color
+
+			setTimeout(function(){
+				console.log("test2");
+				//TODO: Show a go back button
+			}, 1000);
+		}, 2000);
+	});
+});
