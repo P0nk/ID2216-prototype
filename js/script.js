3f085e299152b9c3a4fdf10013d545eb8f1a94d8
Per Lovén <loven.per@gmail.com>
2018-11-19 14:27:39 +0100

    Change to iframe player

--- js/script.js
+++ js/script.js
@@ -1,18 +1,65 @@
+/*
+$(document).ready(function(){
+	function onYouTubeIframeAPIReady() {
+		player = new YT.Player('player', {
+			height: '390',
+			width: '640',
+			videoId: 'M7lc1UVf-VE',
+			events: {
+				'onReady': onPlayerReady,
+				'onStateChange': onPlayerStateChange
+			}
+		});
+}})
+
+// 2. This code loads the IFrame Player API code asynchronously.
+      var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      // 3. This function creates an <iframe> (and YouTube player)
+      //    after the API code downloads.
+      var player;
+
+      }
+
+      // 4. The API will call this function when the video player is ready.
+      function onPlayerReady(event) {
+        event.target.playVideo();
+      }
+
+      // 5. The API calls this function when the player's state changes.
+      //    The function indicates that when playing a video (state=1),
+      //    the player should play for six seconds and then stop.
+      var done = false;
+      function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING && !done) {
+          setTimeout(stopVideo, 6000);
+          done = true;
+        }
+      }
+      function stopVideo() {
+        player.stopVideo();
+      }
+
 function showSearchItems(){
 	document.getElementById("searchedItemsList").style.display = "block";
 }
-
+*/
 
 $(document).on("pageinit", function() {
 	$('video').on("play", function() {
 		setTimeout(function(){
 			console.log("test");
 			//TODO: change the progress bar color
-
+			document.getElementById("rewindBtn1").innerHTML = "0:10-";
+			document.getElementById("rewindBtn1").style.display = "block";
 			setTimeout(function(){
 				console.log("test2");
 				//TODO: Show a go back button
-			}, 1000);
+			}, 10000);
 		}, 2000);
 	});
 });
